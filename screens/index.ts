480d17488aea1636cae796d54581bc9b32a34a56
ntmk202 <khue131@gmail.com>
2024-08-30 01:52:49 +0700

    password screen

--- screens/index.ts
+++ screens/index.ts
@@ -1,2 +1,5 @@
 // auth screen
 export {default as Signin} from './signin/SigninScreen'
+export {default as SignUp} from './signup/SignupScreen'
+export {default as VerifyEmail} from './password/VerifyEmailScreen'
+export {default as UpdatePassword} from './password/UpdatePasswordScreen'
